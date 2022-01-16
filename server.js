472f9ab417e01a0f1b346d7a469a9995e746fac1
adrianoadvrio <adrianoadvrio@yahoo.com.br>
2022-01-16 13:58:21 -0300

    ready to connect mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let express = require("express")
-
 let app = express()
+
+app.use(express.urlencoded({extended: false}))
 
 app.get("/", function(req, res) {
 
@@ -18,9 +19,9 @@ res.send(`
     <h1 class="display-4 text-center py-1">To-Do App</h1>
     
     <div class="jumbotron p-3 shadow-sm">
-      <form>
+      <form action="/creat-item" method="POST"> 
         <div class="d-flex align-items-center">
-          <input autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+          <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
           <button class="btn btn-primary">Add New Item</button>
         </div>
       </form>
@@ -58,7 +59,12 @@ res.send(`
 
 })
 
-app.post()
+app.post('/creat-item', function(req, res){
+
+    console.log(req.body.item)
+    res.send('Thanks for submitting the form.')
+
+} )
 
 app.listen(3000)
 
