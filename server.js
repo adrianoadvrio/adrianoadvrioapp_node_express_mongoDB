1e466bcc6b85dad0b22d7c0cd859af06088ae26b
adrianoadvrio <adrianoadvrio@yahoo.com.br>
2022-01-17 11:18:11 -0300

    add and reading from DB complete

--- server.js
+++ server.js
@@ -17,65 +17,55 @@ app.listen(3000)
 app.use(express.urlencoded({extended: false}))
 
 app.get("/", function(req, res) {
-
-res.send(`
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>AGENDA - NODE JS  - MONGODB CLOUD AWS</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-</head>
-<body>
-  <div class="container">
-    <h3 class="display-5 text-center py-1">AGENDA - NODE JS  - MONGODB CLOUD AWS</h3>
+db.collection('items').find().toArray(function(err, items){
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>AGENDA - NODE JS  - MONGODB CLOUD AWS</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    </head>
+    <body>
+      <div class="container">
+        <h3 class="display-5 text-center py-1">AGENDA - NODE JS  - MONGODB CLOUD AWS</h3>
+        
+        <div class="jumbotron p-3 shadow-sm">
+          <form action="/creat-item" method="POST"> 
+            <div class="d-flex align-items-center">
+              <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+              <button class="btn btn-primary">Add Nuevo</button>
+            </div>
+          </form>
+        </div>
+        
+        <ul class="list-group pb-5">
+          ${items.map(function(item){
+              return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+              <span class="item-text">${item.text}</span>
+              <div>
+                <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+                <button class="delete-me btn btn-danger btn-sm">Borrar</button>
+              </div>
+            </li>`
+          }).join('')}          
+        </ul>
+        
+      </div>
+      
+    </body>
+    </html>
+    `)
     
-    <div class="jumbotron p-3 shadow-sm">
-      <form action="/creat-item" method="POST"> 
-        <div class="d-flex align-items-center">
-          <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
-          <button class="btn btn-primary">Add Nuevo</button>
-        </div>
-      </form>
-    </div>
-    
-    <ul class="list-group pb-5">
-      <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-        <span class="item-text">Fake example item #1</span>
-        <div>
-          <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-          <button class="delete-me btn btn-danger btn-sm">Borrar</button>
-        </div>
-      </li>
-      <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-        <span class="item-text">Fake example item #2</span>
-        <div>
-          <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-          <button class="delete-me btn btn-danger btn-sm">Borrar</button>
-        </div>
-      </li>
-      <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-        <span class="item-text">Fake example item #3</span>
-        <div>
-          <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-          <button class="delete-me btn btn-danger btn-sm">Borrar</button>
-        </div>
-      </li>
-    </ul>
-    
-  </div>
-  
-</body>
-</html>
-`)
+})
 
 })
 
 app.post('/creat-item', function(req, res){
 
     db.collection('items').insertOne({text:req.body.item}, function(){
-    res.send('Thanks for submitting the form.')
+    res.redirect('/')
     })
     
 
